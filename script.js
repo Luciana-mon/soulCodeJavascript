75f51967ac72227c76b28075e739817c1946488f
Luciana-mon <luciana.monsantos@gmail.com>
2023-02-21 22:20:15 -0300

    resolução do exercicio array

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ Defina valores para as variáveis: nome do produto, preço do produto, desconto 
 
 Caso desconto válido seja verdadeiro. Realize o cálculo do desconto e salve em uma nova variável: preço final. Caso contrário, apenas repasse o valor do produto para variável de preço final;
 
-Faça a seguinte verificação: Se o preço final do produto for maior que 1000, reduza 50 reais. Caso contrário, apenas mostre uma mensagem na tela com o valor do preço final;
-
-Crie um array de demandas com os valores: 10, 2, 1, 30, 5. Cada item no array representa a demanda de um cliente diferente. Por exemplo o primeiro cliente está solicitando 10 produtos.
-
-Para cada demanda no array calcule quanto cada cliente deverá pagar e mostre a mensagem no formato: "O cliente 1 deverá pagar: R$ 585". Salve o total de cada cliente em um novo array que deverá começar vazio.*/
+Faça a seguinte verificação: Se o preço final do produto for maior que 1000, reduza 50 reais. Caso contrário, apenas mostre uma mensagem na tela com o valor do preço final; */
 
 let nomeProduto = "Geladeira";
 let precoProduto = 1100.00;
@@ -29,6 +25,35 @@ if (precoFinal >= 1000) {
     precoFinal = precoProduto
     console.log(`Preco do produto sem desconto R$ ${precoFinal.toFixed(2)}`);
 }
+
+
+console.log("*****************************************************************")
+
+/*
+Crie um array de demandas com os valores: 10, 2, 1, 30, 5. Cada item no array representa a demanda de um cliente diferente. Por exemplo o primeiro cliente está solicitando 10 produtos.
+
+Para cada demanda no array calcule quanto cada cliente deverá pagar e mostre a mensagem no formato: "O cliente 1 deverá pagar: R$ 585". Salve o total de cada cliente em um novo array que deverá começar vazio.*/
+
+let demanda = [10, 2, 1, 30, 5]
+let cliente = [];
+let produto = 50;
+
+let valor = demanda.map(function (e, cliente) {
+    return e * produto
+    console.log(`${cliente + 1}`)
+})
+
+console.log(valor);
+
+
+
+
+
+
+
+
+
+
 
 
 
